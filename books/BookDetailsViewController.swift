bfe6d250a673989dcf3e9d38ee3ab4c44f835c82
Andrew Bennet <me@andrewbennet.com>
2016-04-30 14:12:38 +0100

    Work on the Edit Book page.

--- books/BookDetailsViewController.swift
+++ books/BookDetailsViewController.swift
@@ -23,29 +23,10 @@ class BookDetailsViewController: UIViewController {
         updateUi()
     }
     
-    @IBAction func moreIsPressed(sender: UIBarButtonItem) {
-        // We are going to show an action sheet
-        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
-        
-        // For a BookReadState, adds the option
-        func addOptionToSwitchToState(state: BookReadState){
-            optionMenu.addAction(pageActions.forReadState(state).makeUIAlertAction{self.switchState(state)})
-        }
-        
-        // Add the options to switch read state. We will exclude the state we are currenly in.
-        [.Reading, .ToRead, .Finished].filter(){$0 != book?.readState}.forEach(addOptionToSwitchToState)
-        
-        // Always add the delete and cancel options
-        optionMenu.addAction(pageActions.delete.makeUIAlertAction{self.delete()})
-        optionMenu.addAction(pageActions.cancel.makeUIAlertAction{})
-        
-        // Bring up the action sheet
-        self.presentViewController(optionMenu, animated: true, completion: nil)
-    }
-    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "editBookSegue"{
-            let editBookController = segue.destinationViewController as! EditBookViewController
+            let navController = segue.destinationViewController as! UINavigationController
+            let editBookController = navController.viewControllers.first as! EditBookViewController
             editBookController.book = self.book
         }
     }
@@ -76,59 +57,5 @@ class BookDetailsViewController: UIViewController {
             imageView.image = nil
         }
         descriptionLabel.text = book?.bookDescription
-    }
-    
-    /// Combines all the possible actions which this page can show
-    enum pageActions{
-        case markAsReading
-        case markAsToRead
-        case markAsFinished
-        case delete
-        case cancel
-        
-        private var titleText: String{
-            switch self{
-            case .markAsReading:
-                return "Mark as Reading"
-            case .markAsToRead:
-                return "Mark as To Read"
-            case .markAsFinished:
-                return "Mark as Finished"
-            case .delete:
-                return "Delete"
-            case .cancel:
-                return "Cancel"
-            }
-        }
-        
-        private var style: UIAlertActionStyle! {
-            switch self{
-            case .delete:
-                return .Destructive
-            case .cancel:
-                return .Cancel
-            default:
-                return .Default
-            }
-        }
-        
-        static func forReadState(state: BookReadState) -> pageActions {
-            switch state{
-            case .Reading:
-                return .markAsReading
-            case .ToRead:
-                return .markAsToRead
-            case .Finished:
-                return .markAsFinished
-            }
-        }
-        
-        func makeUIAlertAction(bookFunction: (Void -> Void)) -> UIAlertAction{
-            // Wrap the supplied function in one whose signature matches the UIAlertAction constructor
-            func bookFunctionWithInput(_: UIAlertAction) -> Void{
-                bookFunction()
-            }
-            return UIAlertAction(title: titleText, style: style, handler: bookFunctionWithInput)
-        }
     }
 }
--- books/EditBookViewController.swift
+++ books/EditBookViewController.swift
@@ -13,10 +13,7 @@ import UIKit
 class EditBookViewController: FormViewController {
     
     var book: Book!
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-    }
+    @IBOutlet weak var doneButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +22,80 @@ class EditBookViewController: FormViewController {
         bookDetailsSection.append(SegmentedRow<BookReadState>("book-read-state") {
             $0.options = [.ToRead, .Reading, .Finished]
             $0.value = book.readState
-            })
+        })
         bookDetailsSection.append(TextRow("title") {
             $0.placeholder = "Title"
             $0.value = book.title
-            }.onChange{ _ in
-                self.setStateOfDoneButton()
-            })
+        }.onChange{_ in
+            self.setStateOfDoneButton()
+        })
         bookDetailsSection.append(TextRow("author") {
             $0.placeholder = "Author"
             $0.value = book.authorList
-            }.onChange{ _ in
-                self.setStateOfDoneButton()
-            })
+        }.onChange{ _ in
+            self.setStateOfDoneButton()
+        })
         bookDetailsSection.append(TextAreaRow("description") {
             $0.placeholder = "Description"
             $0.value = book.bookDescription
-            })
+        })
         form.append(bookDetailsSection)
+        
+        let deleteSection = Section()
+        deleteSection.append(ButtonRow("delete"){
+            $0.title = "Delete Book"
+        }.onCellSelection{ _ in
+            // We are going to show an action sheet
+            let confirmDeleteAlert = UIAlertController(title: "Delete?", message: nil, preferredStyle: .ActionSheet)
+            
+            // Bring up the action sheet)
+            confirmDeleteAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+            confirmDeleteAlert.addAction(UIAlertAction(title: "Delete", style: .Destructive) { _ in
+                appDelegate.booksStore.DeleteBookAndDeindex(self.book)
+                self.navigationController?.popViewControllerAnimated(true)
+                // TODO: Dismiss the *other* navigation controller.
+            })
+            self.presentViewController(confirmDeleteAlert, animated: true, completion: nil)
+        })
+        form.append(deleteSection)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // TODO: The setting of the values should be done here, not in viewDidLoad().
+    }
+    
+    @IBAction func cancelButtonWasPressed(sender: AnyObject) {
+        self.view.endEditing(true)
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func doneButtonWasPressed(sender: AnyObject) {
+        self.view.endEditing(true)
+        let formValues = form.values()
+        
+        // Update the book object from the form values
+        book.readState = formValues["book-read-state"] as! BookReadState
+        book.title = formValues["title"] as! String
+        book.authorList = formValues["author"] as? String
+        book.bookDescription = formValues["description"] as? String
+        
+        // Save the book and dismiss this view.
+        appDelegate.booksStore.Save()
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func setStateOfDoneButton() {
+        let formValues = form.values()
         
+        if (formValues["title"] as? String)?.isEmpty ?? true{
+            doneButton.enabled = false
+        }
+        else if (formValues["author"] as? String)?.isEmpty ?? true{
+            doneButton.enabled = false
+        }
+        else{
+            doneButton.enabled = true
+        }
     }
 }
