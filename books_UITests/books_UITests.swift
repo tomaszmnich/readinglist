a4059e8b67856718e156464e82e3a77e9b9343e6
Andrew Bennet <me@andrewbennet.com>
2016-10-27 23:58:52 +0100

    Rejig UITests a little.

--- books_UITests/books_UITests.swift
+++ books_UITests/books_UITests.swift
@@ -20,31 +20,9 @@ class books_UITests: XCTestCase {
         super.tearDown()
     }
     
-    func testInitialTables() {
-        let app = XCUIApplication()
-        let tabBarsQuery = app.tabBars
-        
-        XCTAssert(app.tabBars.buttons["Reading"].isSelected)
-        XCTAssertEqual(app.tables.cells.count, 0)
-
-        tabBarsQuery.buttons["Finished"].tap()
-        XCTAssertEqual(app.tables.cells.count, 0)
-        
-        tabBarsQuery.buttons["Settings"].tap()
-        XCTAssertNotEqual(app.tables.cells.count, 0)
-    }
-    
-    func testAddManualBook() {
-        let app = XCUIApplication()
+    private func createManualBook(app: XCUIApplication) {
         let nextButton = app.toolbars.children(matching: .button).element(boundBy: 1)
-        
-        app.tabBars.buttons["Reading"].tap()
-        
-        app.navigationBars["Reading"].buttons["Add"].tap()
-        app.sheets.buttons["Enter Manually"].tap()
-        
         app.textFields["Title"].tap()
-        sleep(1)
         app.typeText("The Catcher in the Rye")
         
         nextButton.tap()
@@ -57,6 +35,15 @@ class books_UITests: XCTestCase {
         app.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "July")
         app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "16")
         app.pickerWheels.element(boundBy: 2).adjust(toPickerWheelValue: "1951")
+    }
+    
+    func testAddManualBook() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Reading"].tap()
+        app.navigationBars["Reading"].buttons["Add"].tap()
+        app.sheets.buttons["Enter Manually"].tap()
+        
+        createManualBook(app: app)
         
         app.navigationBars["Add Manually"].buttons["Next"].tap()
         app.navigationBars["Set Read State"].buttons["Done"].tap()
