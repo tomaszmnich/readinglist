8ff7dff274f831ee40dba0bd2fcd6907791855f1
Andrew Bennet <me@andrewbennet.com>
2017-03-05 23:33:43 +0000

    Try to get travis to run tests

--- books_UITests/ManualBook.swift
+++ books_UITests/ManualBook.swift
@@ -7,6 +7,16 @@
 //
 
 import XCTest
+
+extension XCUIElement {
+    // The following is a workaround for inputting text in the simulator when the keyboard is hidden
+    func setText(_ text: String, application: XCUIApplication) {
+        UIPasteboard.general.string = text
+        doubleTap()
+        application.menuItems["Paste"].tap()
+    }
+}
+
 class books_UITests: XCTestCase {
         
     override func setUp() {
@@ -21,43 +31,41 @@ class books_UITests: XCTestCase {
     
     func testAddAndDeleteManualBook() {
         let app = XCUIApplication()
+        let initialNumberOfCells = app.tables.cells.count
+        
+        // Get to the Enter Manually page
         app.tabBars.buttons["Reading"].tap()
         app.navigationBars["Reading"].buttons["Add"].tap()
         app.sheets.buttons["Enter Manually"].tap()
         
-        createManualBook(app: app)
-        sleep(1)
-        XCTAssertEqual(app.tables.cells.count, 1)
-        
-        app.tables.children(matching: .cell).element(boundBy: 0).tap()
-        deleteBook(app: app)
-        sleep(1)
-        XCTAssertEqual(app.tables.cells.count, 0)
-    }
-    
-    private func createManualBook(app: XCUIApplication) {
-        app.textFields["Title"].tap()
-        app.typeText("The Catcher in the Rye")
-        
+        // Add some book metadata
         let nextButton = app.toolbars.children(matching: .button).element(boundBy: 1)
+        app.textFields["Title"].setText("The Catcher in the Rye", application: app)
+        app.textFields["Author"].setText("J. D. Salinger", application: app)
         nextButton.tap()
-        app.typeText("J. D. Salinger")
-        
-        nextButton.tap()
-        app.typeText("214")
-        
+        app.typeText("241")
         nextButton.tap()
         app.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "July")
         app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "16")
         app.pickerWheels.element(boundBy: 2).adjust(toPickerWheelValue: "1951")
         
+        // Add reading state
         app.navigationBars["Add Manually"].buttons["Next"].tap()
-        app.navigationBars["Set Read State"].buttons["Done"].tap()
-    }
-    
-    private func deleteBook(app: XCUIApplication) {
+        app.navigationBars["The Catcher in the Rye"].buttons["Done"].tap()
+        sleep(1)
+        
+        // Verify we have 1 more row
+        let oneMoreRow = UInt(initialNumberOfCells + 1)
+        XCTAssertEqual(app.tables.cells.count, oneMoreRow)
+        
+        // Delete the book
+        app.tables.children(matching: .cell).element(boundBy: initialNumberOfCells).tap()
         app.navigationBars.buttons["Edit"].tap()
         app.tables.staticTexts["Delete Book"].tap()
         app.sheets.buttons["Delete"].tap()
+        sleep(1)
+        
+        // Verify the row has gone
+        XCTAssertEqual(app.tables.cells.count, initialNumberOfCells)
     }
 }
