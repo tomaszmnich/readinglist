5cead56672ac837fa18dc5d29db6315238bef710
Andrew Bennet <me@andrewbennet.com>
2017-09-14 09:00:41 +0100

    Change SVProgressHUD gradient

--- books/AppDelegate.swift
+++ books/AppDelegate.swift
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Prepare the progress display style. Switched to dark in 1.4 due to a bug in the display of light style
         SVProgressHUD.setDefaultStyle(.dark)
         SVProgressHUD.setDefaultAnimationType(.native)
-        SVProgressHUD.setDefaultMaskType(.gradient)
+        SVProgressHUD.setDefaultMaskType(.clear)
         SVProgressHUD.setMinimumDismissTimeInterval(2)
 
         return true
